7129226f724a3c81e5a4567e09867f04a0e8edd2
Morais Len (Win) <moraislenf@gmail.com>
2021-10-30 14:02:59 -0300

    InitialCommit

---
+++ Controllers/ConvertController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiConvertTemperature.Enties;
+
+namespace WebApiConvertTemperature.Controllers
+{
+    [ApiController]
+    [Route("api/v1/convertTemperature/")]
+    public class ConvertController : Controller
+    {
+        [HttpPost]
+        public ActionResult<dynamic> Convert(ConvertEntie _obj)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            switch (_obj.In + "|" + _obj.To)
+            {
+                case "celsius|fahrenheit":
+                    return Ok(_obj.ValueConvert * 1.8 + 32);
+                case "celsius|kelvin":
+                    return Ok(_obj.ValueConvert + 273.15);
+
+                case "fahrenheit|celsius":
+                    return Ok((_obj.ValueConvert - 32) * 5 / 9);
+                case "fahrenheit|kelvin":
+                    return Ok((_obj.ValueConvert - 32) * 5 / 9 + 273.15);
+
+                case "kelvin|celsius":
+                    return Ok(_obj.ValueConvert - 273.15);
+                case "kelvin|fahrenheit":
+                    return Ok((_obj.ValueConvert - 273.15) * 9 / 5 + 32);
+            }
+
+            return NotFound();
+        }
+
+    }
+}
---
+++ Enties/ConvertEntie.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApiConvertTemperature.Enties
+{
+    public class ConvertEntie
+    {
+        public float ValueConvert { get; set; }
+        public string In { get; set; }
+        public string To { get; set; }
+    }
+}
